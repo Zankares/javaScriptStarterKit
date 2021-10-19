55e0441a9500a439d83e39965e9fdfe2d3f0ae08
Zankares <serkansoybas@gmail.com>
2021-10-19 10:52:33 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba")
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+let dolarDun = 9.2
+let dolarBugun = 9.3
+console.log("<ul>")
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+}
+
+console.log("</ul>")
+    
+{
+    let dolarDun = 9.1
+}
+
+console.log(dolarDun)
+
+
+
+console.log(konutKredileri)
